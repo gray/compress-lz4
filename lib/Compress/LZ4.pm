5de41e4f66e7d3715ff3c10711061e27607f0720
gray <gray@cpan.org>
2013-03-18 16:34:18 -0700

    update copyright dates

--- lib/Compress/LZ4.pm
+++ lib/Compress/LZ4.pm
@@ -145,7 +145,7 @@ L<http://search.cpan.org/dist/Compress-LZ4/>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2012 gray <gray at cpan.org>, all rights reserved.
+Copyright (C) 2012-2013 gray <gray at cpan.org>, all rights reserved.
 
 This library is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself.
